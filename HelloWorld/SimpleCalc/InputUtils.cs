fa09b2eadffbf7b3702630c321e0f3addc8d40df
éZekira <zekiramabanga@gmail.com>
2020-01-24 17:50:43 +0100

    End work simple Calc

---
+++ HelloWorld/SimpleCalc/InputUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    public class InputUtils
+    {
+        public static decimal GetIntputDecimal(string message)
+        {
+            decimal number;
+            Console.Write(message);
+            string numberAsString = Console.ReadLine();
+            while (!decimal.TryParse(numberAsString, out number))
+            {
+                Console.Clear();
+                Console.Write("A number: ");
+                numberAsString = Console.ReadLine();
+            }
+            return number;
+        }
+    }
+}
--- HelloWorld/SimpleCalc/Program.cs
+++ HelloWorld/SimpleCalc/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SimpleCalc
 {
     class Program
@@ -12,37 +13,14 @@ namespace SimpleCalc
         {
             decimal FDec;
             decimal SDec;
-            Console.Write("First number: ");
-            string FNum=Console.ReadLine();
-            while (!decimal.TryParse(FNum, out FDec))
-            {
-                Console.Write("A number: ");
-                FNum = Console.ReadLine();
-            }
-            Console.Write("Second number: ");
-            string SNum = Console.ReadLine();
-            while (!decimal.TryParse(SNum, out SDec))
-            {
-                Console.Write("A number: ");
-                SNum = Console.ReadLine();
-            }
 
-            Console.WriteLine();
-            Console.WriteLine($"you wrote {FNum} and {SNum}");
+            FDec = InputUtils.GetIntputDecimal("First number: ");
+            SDec = InputUtils.GetIntputDecimal("Second number: ");
+
+            Console.Clear();
+            Console.WriteLine($"you wrote {FDec} and {SDec}");
             Console.WriteLine("The answer:  {0}",FDec+SDec);
             Console.ReadKey();
-        }
-
-        static decimal checkNumber(string s)
-        {
-            decimal d = 0.00m;
-            //string s = "";
-            while (!decimal.TryParse(s, out d))
-            {
-                Console.Write("A number: ");
-                s = Console.ReadLine();
-            }
-            return d;
         }
     }
 }
