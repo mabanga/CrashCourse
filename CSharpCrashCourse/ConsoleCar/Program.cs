0dfa9fe98e10be253f9955b6df11b1f729e3adb0
Zekira Mabanga <zekiramabanga@gmail.com>
2020-01-27 09:47:36 +0100

    Add Class Car

---
+++ CSharpCrashCourse/ConsoleCar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilsModels;
+namespace ConsoleCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Toyota");
+            car.Color = "White";
+            car.LicenceNumber = "ABC1234";
+            Console.WriteLine(car);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharpCrashCourse/UtilsModels/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilsModels
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Color { get; set; }
+        public string LicenceNumber { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"LicenceNumber \t: {LicenceNumber}");
+            sb.AppendLine($"Make \t\t: {Make}");
+            sb.AppendLine($"Color \t\t: {Color}");
+            sb.AppendLine($"Year \t\t: {Year}");
+            return sb.ToString();
+        }
+        public Car(string make)
+        {
+            Make = make;
+        }
+    }
+}
