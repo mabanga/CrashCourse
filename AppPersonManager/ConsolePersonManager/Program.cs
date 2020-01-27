9aee75efb8b45f0d3275db100b84d84b65ff5356
Zekira Mabanga <zekiramabanga@gmail.com>
2020-01-27 15:09:19 +0100

    Starting PersonManager App

---
+++ AppPersonManager/ConsolePersonManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using LibraryDLL;
+using LibraryModels;
+namespace ConsolePersonManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+        }
+    }
+}
