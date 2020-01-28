3943236a28f4ef8bab4cd24924eda13eeac694c2
Zekira Mabanga <zekiramabanga@gmail.com>
2020-01-28 10:11:55 +0100

    Je suis

---
+++ AppPersonManager/LibraryDLL/Facade.cs
@@ -0,0 +1,15 @@
+﻿using LibraryDLL.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryDLL
+{
+    public class Facade
+    {
+        public IPersonManager GetPersonManager()
+        {
+            return new PersonManagerFakeDB();
+        }
+    }
+}
